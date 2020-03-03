fa3940d379b584892277b2f50231856ce138139f
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-03 22:53:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,22 +2,22 @@
 
 def countdown(number)
   while number>0 do
-    puts"#{number} SECOND(S) !"
+    puts"#{number} SECOND(S)!"
     number-=1
   end
   
-  return "HAPPY NEW YEAR !"
+  return "HAPPY NEW YEAR!"
   
 end
 
 
 def countdown_with_sleep(number)
   while number>0 do
-    puts"#{number} SECOND(S) !"
+    puts"#{number} SECOND(S)!"
     number-=1
     sleep 1
   end
   
-  return "HAPPY NEW YEAR !"
+  return "HAPPY NEW YEAR!"
 end
   
