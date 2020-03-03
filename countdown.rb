52e770a435f6c6faefea122d49eee66d92c9c227
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-03 22:52:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ def countdown(number)
     number-=1
   end
   
-  return "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR !"
   
 end
 
@@ -18,6 +18,6 @@ def countdown_with_sleep(number)
     sleep 1
   end
   
-  return "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR !"
 end
   
